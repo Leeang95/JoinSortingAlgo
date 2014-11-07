c7624dfa47606d8fe6ec47f5a3cbcabd5d11861b
Milinx <milint18@gmail.com>
2014-11-06 17:27:44 -0800

    Latest Backup V2

--- Joins/src/SortMerge.cpp
+++ Joins/src/SortMerge.cpp
@@ -47,7 +47,6 @@ Status SortMerge::Execute(JoinSpec& left, JoinSpec& right, JoinSpec& out) {
 	////// ***** Phase 2: Merging ***** //////
 	RecordID leftrid, rightrid, insertedrid;
 	int *leftJoinAttr, *rightJoinAttr;
-	int *pleftJoinAttr, *prightJoinAttr;
 	char *leftarr = new char[left.recLen];
 	char *rightarr = new char[right.recLen];
 	char *joinedarr = new char[out.recLen];
@@ -73,35 +72,11 @@ Status SortMerge::Execute(JoinSpec& left, JoinSpec& right, JoinSpec& out) {
 			// Create the record and insert into tmpHeap...
 			MakeNewRecord(joinedarr,leftarr,rightarr,left,right);
 			stat = tmpHeap->InsertRecord(joinedarr,out.recLen,insertedrid); if (stat != OK) {std::cout << "TRACER    5" << std::endl; return FAIL;}
-		
-			// Continue searching for matching tuples on the right and insert them into tmpHeap.
-			stat = rightScan->GetNext(rightrid,rightarr,right.recLen); if (stat != OK) {std::cout << "TRACER    6" << std::endl; return FAIL;}
-			prightJoinAttr = (int*)(rightarr + right.offset);
-			while (!rightScan->noMore && *leftJoinAttr == *prightJoinAttr) {
-				//### It never goes inside this loop ### //
-				MakeNewRecord(joinedarr,leftarr,rightarr,left,right);
-			    stat = tmpHeap->InsertRecord(joinedarr,out.recLen,insertedrid); if (stat != OK) {std::cout << "TRACER    7" << std::endl; return FAIL;}
-                stat = rightScan->GetNext(rightrid,rightarr,right.recLen); if (stat != OK) {std::cout << "TRACER    8" << std::endl; return FAIL;}
-			    prightJoinAttr = (int*)(rightarr + right.offset);
-				std::cout << "TRACERa" << std::endl;
-			}
-
-			// Continue searching for matching tuples on the left and insert them into tmpHeap.
-			stat = leftScan->GetNext(leftrid,leftarr,left.recLen); if (stat != OK) {std::cout << "TRACER    9" << std::endl; return FAIL;}
-		    pleftJoinAttr = (int*)(leftarr + left.offset);
-			while (!leftScan->noMore && *pleftJoinAttr == *rightJoinAttr) {
-				MakeNewRecord(joinedarr,leftarr,rightarr,left,right);
-			    stat = tmpHeap->InsertRecord(joinedarr,out.recLen,insertedrid); if (stat != OK) {std::cout << "TRACER    10" << std::endl; return FAIL;}
-                stat = leftScan->GetNext(leftrid,leftarr,left.recLen); if (stat != OK) {std::cout << "TRACER    11" << std::endl; return FAIL;}
-			    pleftJoinAttr = (int*)(leftarr + left.offset);
-				std::cout << "TRACERb" << std::endl;
-			}
-			//std::cout << "TRACERz" << std::endl;
+		    stat = leftScan->GetNext(leftrid,leftarr,left.recLen); if (stat != OK) {std::cout << "TRACER    12" << std::endl; return FAIL;}
+	    	leftJoinAttr = (int*)(leftarr + left.offset); 
+	    	stat = rightScan->GetNext(rightrid,rightarr,right.recLen); if (stat != OK) {std::cout << "TRACER    13" << std::endl; return FAIL;}
+		    rightJoinAttr = (int*)(rightarr + right.offset);
 		}
-		if (!leftScan->noMore) stat = leftScan->GetNext(leftrid,leftarr,left.recLen); if (stat != OK) {std::cout << "TRACER    12" << std::endl; return FAIL;}
-		leftJoinAttr = (int*)(leftarr + left.offset); 
-		if (!rightScan->noMore) stat = rightScan->GetNext(rightrid,rightarr,right.recLen); if (stat != OK) {std::cout << "TRACER    13" << std::endl; return FAIL;}
-		rightJoinAttr = (int*)(rightarr + right.offset);
 	}
 	
 	out.file = tmpHeap;
@@ -110,3 +85,26 @@ Status SortMerge::Execute(JoinSpec& left, JoinSpec& right, JoinSpec& out) {
 	delete[] joinedarr;
 	return OK;
 }
+
+			//// Continue searching for matching tuples on the right and insert them into tmpHeap.
+			//stat = rightScan->GetNext(rightrid,rightarr,right.recLen); if (stat != OK) {std::cout << "TRACER    6" << std::endl; return FAIL;}
+			//prightJoinAttr = (int*)(rightarr + right.offset);
+			//while (!rightScan->noMore && *leftJoinAttr == *prightJoinAttr) {
+			//	  MakeNewRecord(joinedarr,leftarr,rightarr,left,right);
+			//    stat = tmpHeap->InsertRecord(joinedarr,out.recLen,insertedrid); if (stat != OK) {std::cout << "TRACER    7" << std::endl; return FAIL;}
+            //    stat = rightScan->GetNext(rightrid,rightarr,right.recLen); if (stat != OK) {std::cout << "TRACER    8" << std::endl; return FAIL;}
+			//    prightJoinAttr = (int*)(rightarr + right.offset);
+			//	  std::cout << "TRACERa" << std::endl;
+			//}
+
+			//// Continue searching for matching tuples on the left and insert them into tmpHeap.
+			//stat = leftScan->GetNext(leftrid,leftarr,left.recLen); if (stat != OK) {std::cout << "TRACER    9" << std::endl; return FAIL;}
+		    //pleftJoinAttr = (int*)(leftarr + left.offset);
+			//while (!leftScan->noMore && *pleftJoinAttr == *rightJoinAttr) {
+			//	  MakeNewRecord(joinedarr,leftarr,rightarr,left,right);
+			//    stat = tmpHeap->InsertRecord(joinedarr,out.recLen,insertedrid); if (stat != OK) {std::cout << "TRACER    10" << std::endl; return FAIL;}
+            //    stat = leftScan->GetNext(leftrid,leftarr,left.recLen); if (stat != OK) {std::cout << "TRACER    11" << std::endl; return FAIL;}
+			//    pleftJoinAttr = (int*)(leftarr + left.offset);
+			//	  std::cout << "TRACERb" << std::endl;
+			//}
+			////std::cout << "TRACERz" << std::endl;
